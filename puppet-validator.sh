ef834b27367b23edb198aee06cb1d71683527c2d
Javier Palomo Almena <jvrplmlmn@gmail.com>
2013-12-11 20:33:53 +0100

    Added comments

--- puppet-validator.sh
+++ puppet-validator.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+################################################################################
+# - Check syntax errors
+# - Check style errors (requires the puppet-lint gem)
+################################################################################
 puppet parser validate "$1" && puppet-lint "$1"
